842f51da564d8c853ccae5a40b6efbca8a992163
Neles-333323 <333323@student.mboutrecht.nl>
2023-01-09 12:58:55 +0100

    Mankement toevoegen en video gemaakt

--- app/models/Mankementen.php
+++ app/models/Mankementen.php
@@ -30,14 +30,14 @@ class Mankementen extends Controller
         INNER JOIN mankement AS mank
         ON auto.Id = mank.AutoId
         WHERE instructeur.Id = 3
-        ORDER BY mank.Datum ASC');
+        ORDER BY mank.Datum DESC');
         $results = $this->db->resultSet();
         return $results;
     }
 
     public function addMankement($data)
     {
-        $this->db->query('INSERT INTO `mankement` (`Id`, `Datum`, `Mankement`, `AutoId`) VALUES (NULL, :datum, :mankement :AutoId)');
+        $this->db->query('INSERT INTO `mankement` (`Id`, `AutoId`, `Datum`, `Mankement`) VALUES (NULL, :AutoId, :datum, :mankement)');
         $this->db->bind(':datum', date('Y-m-d'));
         $this->db->bind(':mankement', $data['mankement']);
         $this->db->bind(':AutoId', $data['autoId']);
